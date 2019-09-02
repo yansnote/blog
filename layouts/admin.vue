ca18e6b0eea47dd5b8feaed5481e7ac6b57fd722
Yan Naing <tannaing@gmail.com>
2019-09-02 18:00:25 +0630

    Add sidebar component for admin

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>
+  <div id="yansnote">
+    <sidebar />
     <nuxt />
   </div>
 </template>
 
+<script>
+
+import Sidebar from '~/components/admin/sidebar.vue'
+
+export default {
+  components: {
+    Sidebar
+  }
+}
+</script>
+
 <style>
-
-
-
 html {
   height: 100%
 }
