b7d25e068e20b3cb7edd2fee3b6baf2fae1833a4
Yan Naing <tannaing@gmail.com>
2019-09-04 13:45:16 +0630

    Add require files

---
+++ layouts/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminLayout'
+}
+</script>
+
+<style>
+
+  body {
+    background: blue;
+  }
+
+</style>
